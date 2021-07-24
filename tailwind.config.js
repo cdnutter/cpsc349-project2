48d0b22785d289361e779c2f8bc97a8141384904
Scott Park <scottpark44@csu.fullerton.edu>
2021-07-24 16:29:04 -0700

    Added More Custom Content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens:{
+      'tablet': {'max': '640px'},
+
+      'laptop': {'max': '1024px'},
+
+      'desktop': {'max': '1280px'},
+    },
     extend: {},
   },
   variants: {
